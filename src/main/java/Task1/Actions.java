b21b4e6bfd015a45de10e84b99897625a3d78e57
Lefyy <Lefy1337@gmail.com>
2024-09-21 09:34:08 +0300

    perfected jump

--- src/main/java/Task1/Actions.java
+++ src/main/java/Task1/Actions.java
@@ -11,20 +11,21 @@ public class Actions {
 
     public Actions(Entity entity, int x, int ticksFromStart) {
         this.entity = entity;
+        x = x - entity.getWidth() / 2;
 
         x0 = entity.getX();
         y0 = entity.getY();
+
+        direction = (x - x0) > 0;
         parabolaX0 = (x - x0) / 2;
 
         ticksInStart = ticksFromStart;
         ticksInFinish = ticksFromStart + Math.abs(x - x0);
-
-        direction = (x - x0) > 0;
     }
 
     public void entityJump(int ticksFromStart) {
         int dx = direction ? (ticksFromStart - ticksInStart) : -1 * (ticksFromStart - ticksInStart);
-        int dy = (int) (-0.01 * Math.pow(dx, 2) + 0.02 * parabolaX0 * dx);
+        int dy = (int) (-1.0 / Math.abs(parabolaX0) * Math.pow(dx, 2) + 2 * Math.abs(dx));
 
         entity.setX(x0 + dx);
         entity.setY(y0 - dy);
