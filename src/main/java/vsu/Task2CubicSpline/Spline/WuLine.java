ec977a27f8873be5f3fa1d4701864aca9268c0ed
Lefy <Lefy1337@gmail.com>
2024-11-08 08:23:08 +0300

    pixelWriter

--- src/main/java/vsu/Task2CubicSpline/Spline/WuLine.java
+++ src/main/java/vsu/Task2CubicSpline/Spline/WuLine.java
@@ -1,6 +1,7 @@
 package vsu.Task2CubicSpline.Spline;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.PixelWriter;
 import javafx.scene.paint.Color;
 
 
@@ -17,38 +18,32 @@ public class WuLine {
         int dx = x2 - x1;
         int dy = y2 - y1;
         if (dx == 0 || dy == 0) {
+            g.setFill(Color.BLACK);
             g.strokeLine(x1, y1, x2, y2);
             return;
         }
+        PixelWriter pixel = g.getPixelWriter();
         float gradient = 0;
         if (dx > dy) {
             gradient = (float) dy / dx;
             float intery = y1 + gradient;
-            g.setFill(Color.BLACK);
-            g.strokeLine(x1, y1, x1, y1);
+            pixel.setColor(x1, y1, Color.BLACK);
             for (int x = x1; x < x2; ++x) {
-                g.setFill(new Color(0, 0, 0, (255 - fractionalPart(intery) * 255) / 255));
-                g.strokeLine(x, (int) intery, x, (int) intery);
-                g.setFill(new Color(0, 0, 0, fractionalPart(intery)));
-                g.strokeLine(x, (int) intery + 1, x, (int) intery + 1);
+                pixel.setColor(x, (int) intery, new Color(0, 0, 0, (255 - fractionalPart(intery) * 255) / 255));
+                pixel.setColor(x, (int) intery + 1, new Color(0, 0, 0, fractionalPart(intery)));
                 intery += gradient;
             }
-            g.setFill(Color.BLACK);
-            g.strokeLine(x2, y2, x2, y2);
+            pixel.setColor(x2, y2, Color.BLACK);
         } else {
             gradient = (float) dx / dy;
             float interx = x1 + gradient;
-            g.setFill(Color.BLACK);
-            g.strokeLine(x1, y1, x1, y1);
+            pixel.setColor(x1, y1, Color.BLACK);
             for (int y = y1; y < y2; ++y) {
-                g.setFill(new Color(0, 0, 0, (255 - fractionalPart(interx) * 255) / 255));
-                g.strokeLine((int) interx, y, (int) interx, y);
-                g.setFill(new Color(0, 0, 0, fractionalPart(interx)));
-                g.strokeLine((int) interx + 1, y, (int) interx + 1, y);
+                pixel.setColor((int) interx, y, new Color(0, 0, 0, (255 - fractionalPart(interx) * 255) / 255));
+                pixel.setColor((int) interx + 1, y, new Color(0, 0, 0, fractionalPart(interx)));
                 interx += gradient;
             }
-            g.setFill(Color.BLACK);
-            g.strokeLine(x2, y2, x2, y2);
+            pixel.setColor(x2, y2, Color.BLACK);
         }
 
     }
